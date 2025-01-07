cfc936f868c8584b6dcf3bd8e1475022a6c7a592
EmoSyue <146612893+EmoSyue@users.noreply.github.com>
2025-01-07 20:43:12 +0800

    Update fanyi.py

--- fanyi.py
+++ fanyi.py
@@ -4,8 +4,8 @@ import json
 from hashlib import md5
 
 # Set your own appid/appkey.
-appid = 'xxxxx'
-appkey = 'xxxxxxx'
+appid = '20250107002247587'
+appkey = '26ge3Es5y1sFyObpufa5'
 
 # For list of language codes, please refer to `https://api.fanyi.baidu.com/doc/21`
 from_lang = 'zh'
